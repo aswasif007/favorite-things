ad0eb5b56ce26d9824f6f9a01e38e563105cea56
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2019-06-19 22:42:59 +0600

    Use mysql as default database - Read database config from file

--- server/server/settings.py
+++ server/server/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/opt/favorite-things/config.cnf',
+        },
     }
 }
 
