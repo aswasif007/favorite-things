fbebf6daf5bcbaa15d6a7ceea717cec0006aebe3
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2019-06-19 23:44:07 +0600

    Create database models - Item, Category, Rank and AuditLog

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+
+from .utils import generate_uuid, generate_color, color_code_validator
+
+
+class Category(models.Model):
+    guid = models.CharField(max_length=32, primary_key=True, editable=False, default=generate_uuid)
+    title = models.CharField(max_length=64, null=False, blank=False)
+    color_code = models.CharField(max_length=7, default=generate_color, validators=[color_code_validator])
+    created_at = models.DateTimeField(editable=False, default=datetime.now)
+    modified_at = models.DateTimeField(editable=False, default=datetime.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Item(models.Model):
+    guid = models.CharField(max_length=32, primary_key=True, editable=False, default=generate_uuid)
+    title = models.CharField(max_length=64, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    metadata = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(editable=False, default=datetime.now)
+    modified_at = models.DateTimeField(editable=False, default=datetime.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Rank(models.Model):
+    data = models.TextField()
+
+
+class AuditLog(models.Model):
+    log = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(editable=False, default=datetime.now)
+
+    def __str__(self):
+        return self.log
+
+    class Meta:
+        verbose_name_plural = 'Audit Logs'
+        verbose_name = 'Audit Log'
