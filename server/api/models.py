b6b036a7703f25bd7e2604508ebe515ebd0e360c
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2019-06-24 00:45:14 +0600

    Store logs on any modification of Item or Category

--- server/api/models.py
+++ server/api/models.py
@@ -15,6 +15,19 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        super(Category, self).save(*args, **kwargs)
+
+        if self.pk:
+            AuditLog.new_log(f'Category {self.title} updated.')
+        else:
+            AuditLog.new_log(f'Category {self.title} created.')
+
+    def delete(self, *args, **kwargs):
+        super(Category, self).delete(*args, **kwargs)
+
+        AuditLog.new_log(f'Category {self.title} deleted.')
+
     class Meta:
         verbose_name_plural = 'Categories'
         ordering = ['-created_at']
@@ -46,7 +59,12 @@ class Item(models.Model):
         rank_obj.insert_item(self.guid)
         rank_obj.save()
 
-        return super(Item, self).save(*args, **kwargs)
+        super(Item, self).save(*args, **kwargs)
+
+        if self.pk:
+            AuditLog.new_log(f'Item {self.title} updated.')
+        else:
+            AuditLog.new_log(f'Item {self.title} created.')
 
     def delete(self, *args, **kwargs):
         rank_obj = Rank.objects.first()
@@ -54,7 +72,9 @@ class Item(models.Model):
         rank_obj.delete_item(self.guid)
         rank_obj.save()
 
-        return super(Item, self).delete(*args, **kwargs)
+        super(Item, self).delete(*args, **kwargs)
+
+        AuditLog.new_log(f'Item {self.title} deleted.')
 
     class Meta:
         ordering = ['-created_at']
@@ -67,6 +87,10 @@ class Rank(models.Model, RankHandler):
 class AuditLog(models.Model):
     log = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
+
+    @staticmethod
+    def new_log(message):
+        AuditLog.objects.create(log=message)
 
     def __str__(self):
         return self.log
