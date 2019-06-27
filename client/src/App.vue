b9e8262876ca59a41803f4ddf1f7de44876f444a
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2019-06-28 00:33:05 +0600

    Prepare ui scaffolding for the application - Allocate position for the categories, items, search bar, action buttons

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,62 @@
 <template>
-  <div id="app">
-    <hello></hello>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12 position-fixed app-title">
+        Favorite Things
+      </div>
+    </div>
+    <div class="row app-body">
+      <div class="col">
+        <div class="position-fixed">
+          CATEGORIES
+        </div>
+      </div>
+      <div class="col-sm-4 col-md-6 col-lg-8">
+        <div>
+          <main-panel></main-panel>
+        </div>
+      </div>
+      <div class="col">
+        <div class="position-fixed app-logs">
+          LOGS
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
+import MainPanel from './main_panel/MainPanel.vue';
 
 export default {
   name: 'app',
+
   components: {
-    Hello
+    MainPanel,
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "./_variables.scss";
+
+.app-title {
+  font-size: 40px;
+  font-weight: 600;
+  padding: 15px 0;
+  z-index: 10;
+  background: $White;
+}
+
+.app-logs {
+  margin: -20px -15px;
+  padding: 20px 15px;
+  background: $White;
+  width: 100%;
+}
+
+.app-body {
+  padding-top: 100px;
+  height: 1500px;
+}
+</style>
---
+++ client/src/main_panel/MainPanel.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="MainPanel">
+    <div class="row task-bar">
+      <div class="col-8">
+        <div class="position-fixed search-bar">
+          Searchbar
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="float-right">
+          <div class="position-fixed action-bar">
+            Actions
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row item-container">
+      Items
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPanel",
+}
+</script>
+
+<style lang="scss">
+@import "../_variables.scss";
+
+.search-bar {
+  margin: -10px -15px;
+  padding: 10px 15px;
+  background: $White;
+  width: 100%;
+  box-shadow: 2px 4px 4px $LightGray;
+}
+
+.action-bar {
+  margin-left: -50px
+}
+
+.item-container {
+  padding-top: 20px;
+  margin-top: 30px;
+}
+</style>
